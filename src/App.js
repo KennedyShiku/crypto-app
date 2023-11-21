b2498fdc68fdc31622629507d1aae052f5a64969
KennedyShiku <kenrichshiku@gmail.com>
2023-11-21 11:20:42 +0300

    Added Homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import HomePage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Crypto</h1>
-      <h2>This is a crypto app!</h2>
+      <HomePage />
     </div>
   );
 }
